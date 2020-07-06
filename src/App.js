8b18eafcbf2b78f1b94e9ae21cfbbfe1b90f40b3
dongwoo <qorehddn123@naver.com>
2020-07-06 18:04:17 +0900

    first-kakao-maps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route } from 'react-router-dom';
+import MapFood from "./MapFood";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const dashboardRoutes = [];
+
+
+function App(props) {
+
+
+    return (
+        <MapFood/>
+    );
 }
 
 export default App;
